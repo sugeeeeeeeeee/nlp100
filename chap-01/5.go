a7099f253189f7e83445d70b379f6b2c44c74c66
sugeeeeeeeeee@gmail.com <sugeeeeeeeeee@gmail.com>
2018-07-06 12:14:06 +0000

    commit ./chap-01/5.go

---
+++ chap-01/5.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var result []string
+
+func main() {
+	target := "I am an NLPer"
+	kick(target, 2)
+}
+
+func kick(target string, n int) {
+	s1 := strings.Split(target, "")
+	s2 := strings.Split(target, " ")
+	ngram(s1, n)
+	ngram(s2, n)
+}
+
+func ngram(target []string, n int) {
+	max := len(target) - n + 1
+	for i := 0; i < max; i++ {
+		fmt.Printf("%v,", strings.Join(target[i:i+n], ""))
+	}
+	fmt.Printf("\n")
+}
