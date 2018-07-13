f8186e94c205ee39596bb885b8607ed24eadcd65
sugeeeeeeeeee <sugeeeeeeeeee@gmail.com>
2018-07-13 01:27:51 +0000

    commit ./chap-02/13.go

--- chap-02/13.go
+++ chap-02/13.go
@@ -8,32 +8,24 @@ import (
 )
 
 func main() {
-	file, err := os.Open("./chap-02/hightemp.txt")
-	if err != nil {
+	col1, _ := os.Open("./chap-02/col1.txt")
+	col2, _ := os.Open("./chap-02/col2.txt")
+	ret1 := fileread(col1)
+	ret2 := fileread(col2)
+
+	max := len(ret1)
+	var result []string
+	for i := 0; i < max; i++ {
+		result = append(result, strings.Join([]string{ret1[i], ret2[i]}, "\t"))
 	}
-	var ret1 []string
-	var ret2 []string
-	i := 0
-	sc := bufio.NewScanner(file)
+	fmt.Println(strings.Join(result, "\n"))
+}
+
+func fileread(f *os.File) []string {
+	var ret []string
+	sc := bufio.NewScanner(f)
 	for sc.Scan() {
-		ret1 = append(ret1, cut(sc.Text(), 1))
-		ret2 = append(ret2, cut(sc.Text(), 2))
-		i = i + 1
+		ret = append(ret, sc.Text())
 	}
-	output("col1.txt", strings.Join(ret1, "\n"))
-	output("col2.txt", strings.Join(ret2, "\n"))
-}
-
-func cut(s string, n int) string {
-	return strings.Split(s, "\t")[n-1]
-}
-
-func output(filename string, s string) {
-	file, err := os.Create(strings.Join([]string{"./chap-02/", filename}, ""))
-	if err != nil {
-		// Openエラー処理
-	}
-	defer file.Close()
-
-	file.Write(([]byte)(s))
+	return ret
 }
